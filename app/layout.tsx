640f6fba00ecda686b554e0db3fd001e8b65e44f
Johncarlo Ababa <jcababa.drccebu@gmail.com>
2025-02-26 12:57:39 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,12 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Sn0wAlgo - Lightning Fast Jailbreak Algorithms',
+  description: 'Next-generation hyper-speed algorithms for Jailbreak.',
   generator: 'v0.dev',
+  icons: {
+    icon: "/favicon.ico"
+  },
 }
 
 export default function RootLayout({
